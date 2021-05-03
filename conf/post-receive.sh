43f09202858c507331da3dd0ba8a3dbdbab39cb0
Ben Ward <ben@benward.uk>
2021-05-02 19:47:17 -0700

    Add some instructions to the deploy hook having just got it running again

--- conf/post-receive.sh
+++ conf/post-receive.sh
@@ -3,10 +3,15 @@
 # This script is a rather hacky post-receive hook used on my production server to
 # rebuild the site when I commit to a dedicated 'live' repo
 
-# We're about to operate on a different repo clone
+# Before this will function, do the following:
+#
+# Ensure that the `www` group exists, and that you (benward) are a member; otherwise the chmod will fail without sudo
+# Install bundler, and configure it to install gems locally in the project directory
+
+# User this var as we're about to operate on a different repo clone
 unset GIT_DIR
 
-# Redeploy benward.uk Jekyll site
+# Redeploy benward.uk Jekyll site to /var/www/benward
 DEPLOY_LOCATION=/var/www/benward
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
